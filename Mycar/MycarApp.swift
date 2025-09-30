2e9a239c8f5269230eab70ea505c5e4610056825
Alireza <ar.dosthoseini@gmail.com>
2025-09-30 20:10:54 +0200

    Initial Commit

---
+++ Mycar/MycarApp.swift
@@ -0,0 +1,17 @@
+//
+//  MycarApp.swift
+//  Mycar
+//
+//  Created by Alireza on 30/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct MycarApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
